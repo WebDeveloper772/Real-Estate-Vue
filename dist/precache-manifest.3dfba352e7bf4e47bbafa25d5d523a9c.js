5dd31c94c2676701bf12de62c8cebd2ee97a20fe
Carel Williams <carel@live.se>
2021-02-03 14:06:07 +0100

    Dist

---
+++ dist/precache-manifest.3dfba352e7bf4e47bbafa25d5d523a9c.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "76e88eebf0d60319e0e4",
+    "url": "/css/app.3ea729cc.css"
+  },
+  {
+    "revision": "0decc99835f806c445f1",
+    "url": "/css/chunk-vendors.ac3e446b.css"
+  },
+  {
+    "revision": "6f3728fa4b50862129ecf8a40bfbebb6",
+    "url": "/index.html"
+  },
+  {
+    "revision": "76e88eebf0d60319e0e4",
+    "url": "/js/app.c2e78bd1.js"
+  },
+  {
+    "revision": "0decc99835f806c445f1",
+    "url": "/js/chunk-vendors.2b2aabdd.js"
+  },
+  {
+    "revision": "179e825b086dd65f68d2ef2fa016759d",
+    "url": "/manifest.json"
+  },
+  {
+    "revision": "b6216d61c03e6ce0c9aea6ca7808f7ca",
+    "url": "/robots.txt"
+  }
+]);

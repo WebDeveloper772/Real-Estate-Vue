9f6fbb863997f9e822c437efe2a7e6d6641dab17
Carel Williams <carel@live.se>
2021-02-04 19:15:09 +0100

    Restore files

---
+++ precache-manifest.8156f416c2c9dd5423376e008b729e64.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "82c0cab64dec7b1f5ed3",
+    "url": "/css/app.f7f6cc48.css"
+  },
+  {
+    "revision": "aa789c5919b4cd4bf3a0",
+    "url": "/css/chunk-vendors.ac3e446b.css"
+  },
+  {
+    "revision": "63bfae0909d8b4c0d3d1dd5c3b2a3751",
+    "url": "/index.html"
+  },
+  {
+    "revision": "82c0cab64dec7b1f5ed3",
+    "url": "/js/app.4463007b.js"
+  },
+  {
+    "revision": "aa789c5919b4cd4bf3a0",
+    "url": "/js/chunk-vendors.d87fb58c.js"
+  },
+  {
+    "revision": "179e825b086dd65f68d2ef2fa016759d",
+    "url": "/manifest.json"
+  },
+  {
+    "revision": "b6216d61c03e6ce0c9aea6ca7808f7ca",
+    "url": "/robots.txt"
+  }
+]);
